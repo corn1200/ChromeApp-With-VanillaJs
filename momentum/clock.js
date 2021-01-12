ab12bedc2da270624290c306276d692861217807
corn1200 <corn120033@gmail.com>
2021-01-12 22:05:57 +0900

    feat: Date 객체를 생성하고 h1 태그에 현재 시각을 나타나게 한다

--- momentum/clock.js
+++ momentum/clock.js
@@ -1,4 +1,20 @@
-const c = 221
-let a = 221
-c = a - 5
-console.log(b)
+let clockContainer, clockTitle
+
+window.onload = function() {
+    clockContainer = document.querySelector(".js-clock")
+    clockTitle = document.querySelector("h1")
+
+    init()
+}
+
+function getTime() {
+    const date = new Date()
+    const minutes = date.getMinutes()
+    const hours = date.getHours()
+    const seconds = date.getSeconds()
+    clockTitle.innerText = `${hours}:${minutes}:${seconds}`
+}
+
+function init() {
+    getTime()
+}
