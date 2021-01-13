3521b398594b21e214990e149c6f2f5782e7a74d
corn1200 <corn120033@gmail.com>
2021-01-13 10:19:54 +0900

    feat: localstoreage에 json 형태로 저장 후 불러와 list로 출력한다

--- momentum/todolist.js
+++ momentum/todolist.js
@@ -1,4 +1,5 @@
 const TODOS_LS = 'toDos',
+    toDos = [],
     PREV_ONLOAD_TODOLIST = window.onload
 
 let toDoForm, toDoInput, toDoList
@@ -13,27 +14,43 @@ window.onload = function() {
     initToDoList()
 }
 
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos))
+}
+
 function paintToDo(text) {
     const li = document.createElement("li")
     const delBtn = document.createElement("button")
-    delBtn.innerText = "❌"
     const span = document.createElement("span")
+    const newId = toDos.length + 1
+    delBtn.innerText = "❌"
     span.innerText = text
     li.appendChild(delBtn)
     li.appendChild(span)
+    li.id = newId;
     toDoList.appendChild(li)
+    const toDoObj = {
+        text: text,
+        id: newId
+    }
+    toDos.push(toDoObj)
+    saveToDos()
 }
 
 function handleToDoListSubmit(event) {
     event.preventDefault()
     const currentValue = toDoInput.value
     paintToDo(currentValue)
+    toDoInput.value = ""
 }
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS)
-    if (toDos !== null) {
-        
+    const loadToDos = localStorage.getItem(TODOS_LS)
+    if (loadToDos !== null) {
+        const parsedToDos = JSON.parse(loadToDos)
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text)
+        })
     }
 }
 
