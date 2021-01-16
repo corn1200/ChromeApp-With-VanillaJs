89fa302e6547859ef821c5fa4d5491aebbaebdf6
corn1200 <corn120033@gmail.com>
2021-01-16 17:23:43 +0900

    feat: 배경의 지역을 출력한다

--- js/background.js
+++ js/background.js
@@ -1,10 +1,12 @@
-// 랜덤으로 출력할 배경화면의 갯수와
 // 이전 onload를 불러오는 상수
-const IMG_NUMBER = 3,
+const ACCESS_KEY = "jebsyaTCtg0jqVCxl0CvfCmYIRg9UvctXOoLm5LRn0c"
+    UNSPLASH_URL = `https://api.unsplash.com/photos/random/?client_id=${ACCESS_KEY}&query=landscape&orientation=landscape&feature=true`
+    UNUSE_BACKGROUND = "https://source.unsplash.com/random"
+    NONE = "None",
     PREV_ONLOAD_BACKGROUND = window.onload
 
 // body 부분을 할당할 변수
-let body
+let body, locationContainer
 
 // 이전 onload를 실행하고 body를 변수에 할당함
 // 파일에서 담당하는 모든 기능을 실행
@@ -12,29 +14,79 @@ window.onload = function() {
     PREV_ONLOAD_BACKGROUND()
     
     body = document.querySelector("body")
+    locationContainer = document.querySelector(".location")
 
     initBackground()
 }
 
-// 랜덤한 숫자를 받고 랜덤한 이미지를 선택하여
-// 배경화면으로서 출력함
-function paintImage(imgNumber) {
-    const image = new Image()
-    image.src = `https://source.unsplash.com/random`
-    image.classList.add("bgImage")
-    body.append(image)
+function loadBackground() {
+    const savedImage = localStorage.getItem("bg")
+    if (savedImage === null) {
+        getBackgroud()
+    } else {
+        const parsedImage = JSON.parse(savedImage)
+        console.log(parsedImage)
+        const today = new Date()
+        if (today > parsedImage.expiresOn) {
+            getBackgroud()
+        } else {
+            const image = new Image()
+            image.src = parsedImage.url
+            image.classList.add("bgImage")
+            body.appendChild(image)
+
+            parsedImage.name = parsedImage.name !== null ? parsedImage.name : NONE
+            parsedImage.city = parsedImage.city !== null ? parsedImage.city : NONE
+            parsedImage.country = parsedImage.country !== null ? parsedImage.country : NONE
+
+            locationContainer.innerHTML = `${parsedImage.name}, 
+            ${parsedImage.city}, ${parsedImage.country}`
+        }
+    }
+    return
 }
 
-// 랜덤한 숫자를 생성하고 반환함
-function genRandom() {
-    const number = Math.floor(Math.random() * IMG_NUMBER)
-    return number
+function saveBackgroud(imageUrl, city, country, name) {
+    const savedImage = localStorage.getItem("bg")
+    if (savedImage !== null) {
+        localStorage.removeItem("bg")
+    }
+    const expirationDate = new Date()
+    expirationDate.setDate(expirationDate.getDate() + 1)
+    const imageObject = {
+        url: imageUrl,
+        expiresOn: expirationDate,
+        city,
+        country,
+        name
+    }
+    localStorage.setItem("bg", JSON.stringify(imageObject))
+    loadBackground()
+    return
+}
+
+function getBackgroud() {
+    fetch(UNSPLASH_URL)
+        .then(response => response.json())
+        .then(json => {
+            const image = json
+            if (image.urls && image.urls.full && image.location) {
+                const fullurl = image.urls.full
+                const location = image.location
+                const city = location.city
+                const country = location.country
+                const name = location.name
+                saveBackgroud(fullurl, city, country, name)
+            } else {
+                // getBackgroud()
+                body.style.backgroundImage = `url(${UNUSE_BACKGROUND})`
+            }
+        })
+    return
 }
 
 // 파일에서 담당하는 모든 기능을 실행
-// 랜덤한 숫자를 생성함
-// 생성한 숫자에 해당하는 이미지를 출력
 function initBackground() {
-    const randomNumber = genRandom()
-    paintImage(randomNumber)
+    loadBackground()
+    return
 }
